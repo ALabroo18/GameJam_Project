56c2960ddca91e6c1770d10554c3054e38e8f528
Dormio-san <liam.lovett06@gmail.com>
2024-09-13 02:04:56 -0400

    Add player walk animations.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,9 +13,12 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
 
+    private Animator animator;
+
     private void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void Update()
@@ -42,10 +45,54 @@ public class PlayerMovement : MonoBehaviour
         {
             rb2d.velocity = new Vector2(rb2d.velocity.x, rb2d.velocity.y * 0.5f);
         }
+
+        AnimationCheck();
     }
 
     private bool IsGrounded()
     {
         return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    // Changes the animation based on the direction the player is moving.
+    private void AnimationCheck()
+    {
+        if (horizontalInput >= 0.1f)
+        {
+            // Moving right.
+            ChangeAnimatorBools("MoveRight", "MoveLeft", "MoveUp", "MoveDown", "Idle");
+        }
+        else if (horizontalInput <= -0.1f)
+        {
+            // Moving left.
+            ChangeAnimatorBools("MoveLeft", "MoveRight", "MoveUp", "MoveDown", "Idle");
+
+        }
+        else if (verticalInput >= 0.1f)
+        {
+            // Moving up.
+            ChangeAnimatorBools("MoveUp", "MoveLeft", "MoveRight", "MoveDown", "Idle");
+
+        }
+        else if (verticalInput <= -0.1f)
+        {
+            // Moving down.
+            ChangeAnimatorBools("MoveDown", "MoveLeft", "MoveUp", "MoveRight", "Idle");
+        }
+        else
+        {
+            // Not moving.
+            ChangeAnimatorBools("Idle", "MoveLeft", "MoveUp", "MoveDown", "MoveRight");
+        }
+    }
+
+    // Set all animator bools to false besides the one that should be true.
+    private void ChangeAnimatorBools(string trueAnimBool, string falseAnimBool, string falseAnimBool2, string falseAnimBool3, string falseAnimBool4)
+    {
+        animator.SetBool(trueAnimBool, true);
+        animator.SetBool(falseAnimBool, false);
+        animator.SetBool(falseAnimBool2, false);
+        animator.SetBool(falseAnimBool3, false);
+        animator.SetBool(falseAnimBool4, false);
     }
 }
